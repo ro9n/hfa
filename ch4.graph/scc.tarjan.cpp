335f2b08466566def7eeebf043e6765fe2fa4fc3
Touhid Alam <touhid@localhost.localdomain>
2020-04-26 17:50:44 +0600

    scc

--- ch4.graph/scc.tarjan.cpp
+++ ch4.graph/scc.tarjan.cpp
@@ -1,7 +1,3 @@
-/**
- * cut edges / bridges
- * */
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,6 +6,7 @@ using namespace std;
 #define GRAY 1
 #define BLACK 2
 #define fill(c, v) memset(c, v, sizeof c)
+#define eb emplace_back
 
 const int V = 1e4 + 7;
 
@@ -17,29 +14,34 @@ typedef pair<int, int> ii;
 typedef vector<int> vi;
 
 // for weighted grapg vector<ii> g[V]
-vi g[V];
+vi g[V], s;
 
-int t = 0, d[V], l[V], c[V];  // dfs clock, dfs low links, dfs colors
+int t = 0, d[V], l[V], c[V];  // dfs clock, dfs low links, dfs colors, stack to print scc from root
 
 void dfs(int u, int p = -1) {
   l[u] = d[u] = ++t;
   c[u] = GRAY;
+  s.eb(u);
+
   for (int v : g[u]) {
     if (v == p) continue;
     if (c[v] == WHITE) {
       dfs(v, u);
-
-      if (d[u] < l[v]) {
-        cout << u << "->" << v << "is a cut edge";
-      }
-      // tree edge
+    } 
+    
+    if (c[v] == BLACK) {
       l[u] = min(l[u], l[v]);
-    } else {
-      // back edge
-      l[u] = min(l[u], d[v]);
     }
   }
   c[u] = BLACK;
+
+  if(d[u] == l[u]) {
+    // root of scc
+    while(s.back() != u) {
+      cout << s.back() << ' '; s.pop_back(); 
+    }
+    cout << endl;
+  }
 }
 
 int main() {

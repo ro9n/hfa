179ba31e9a9939d2f7038d591df9d723b2885bc8
touhid <taz.touhid@gmail.com>
2020-05-18 12:00:33 +0600

    2.linked.lists

--- lc/msft/2.linked.lists/2.5.merge.two.sorted.lists.cpp
+++ lc/msft/2.linked.lists/2.5.merge.two.sorted.lists.cpp
@@ -2,6 +2,39 @@
 
 using namespace std;
 
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (!l1)
+      return l2;
+    else if (!l2)
+      return l1;
+
+    ListNode *r = new ListNode(0), *h = r;
+
+    while (l1 && l2) {
+      if (l1->val < l2->val) {
+        h->next = new ListNode(l1->val);
+        l1 = l1->next;
+        if (!l1) {
+          h->next->next = l2;
+          break;
+        }
+      } else {
+        h->next = new ListNode(l2->val);
+        l2 = l2->next;
+        if (!l2) {
+          h->next->next = l1;
+          break;
+        }
+      }
+
+      h = h->next;
+    }
+
+    return r->next;
+  }
+};
 
 struct ListNode {
   int val;
--- lc/msft/2.linked.lists/2.7.intersection.of.two.linked.lists.cpp
+++ lc/msft/2.linked.lists/2.7.intersection.of.two.linked.lists.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+class Solution {
+ public:
+  ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    ListNode *a = headA, *b = headB;
+    if (!a || !b) return nullptr;
+    
+    int swp = 2;
+
+    while (a != b) {
+      a = a->next;
+      b = b->next;
+
+      if (!a) {
+        if (swp) {
+          a = headB;
+          --swp;
+        } else
+          return nullptr;
+      }
+      if (!b) {
+        if (swp) {
+          b = headA, --swp;
+        } else
+          return nullptr;
+      }
+    }
+
+    return a;
+  }
+};
 
 struct ListNode {
   int val;
